387a5680a68d7901b5b259a8f498b74889624c72
jimen8930 <jimen8930@aol.com>
2023-09-13 00:05:04 -0400

    Updated the about page with an arrow function and return the about me data.

---
+++ hello-react/src/components/About.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const About = () => {
+    return (
+      <section id="home" className="py-5 text-center d-flex justify-content-center align-items-center">
+        <div className="container">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-md-4">
+              <img src="./avatar-image.png" alt="welcome" className="img-fluid custom-img" />
+            </div>
+            <div className="col-md-4">
+            <h2>About Me</h2>
+              <p>
+              Hello! I'm a full stack software engineer with a background in education. I  bring a diverse skill
+          set and a passion for pushing boundaries both in my professional and
+          personal life.
+              </p>
+              <p>
+              Greetings! I'm a well-rounded software engineer with experience in education. I bring a wide range of skills and a fervor for pushing boundaries in both my work and personal pursuits. With a strong foundation in both front-end and back-end development, I will transform ideas into captivating digital experiences that captivate and engage users.
+              </p>
+              <p>With a background exclusively in education and experience within the school system, I possess a strong foundation in analytical thinking, problem-solving, and meticulous attention to detail. My time within the school system has further enhanced my ability to handle complex data and make informed decisions, emphasizing the importance of effective communication and delivering exceptional experiences for students and colleagues alike.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default About;
